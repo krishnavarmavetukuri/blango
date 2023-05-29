5d7f96c97f556b7ca85414f407fc9cf0d8aed92f
KRISHNA HU21CSEN0300138 <kvetukur@gitam.in>
2023-05-29 10:56:24 +0000

    Finish Serializers

--- blog/api_views.py
+++ blog/api_views.py
@@ -1,3 +1,5 @@
+from blog.api.serializers import PostSerializer
+
 import json
 from http import HTTPStatus
 
@@ -9,6 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 from blog.models import Post
 
 
+""" deleted
 def post_to_dict(post):
     return {
         "pk": post.pk,
@@ -21,17 +24,18 @@ def post_to_dict(post):
         "summary": post.summary,
         "content": post.content,
     }
-
+"""
 
 @csrf_exempt
 def post_list(request):
     if request.method == "GET":
-        posts = Post.objects.all()
-        posts_as_dict = [post_to_dict(p) for p in posts]
-        return JsonResponse({"data": posts_as_dict})
+      posts = Post.objects.all()
+      return JsonResponse({"data": PostSerializer(posts, many=True).data})
     elif request.method == "POST":
         post_data = json.loads(request.body)
-        post = Post.objects.create(**post_data)
+        serializer = PostSerializer(data=post_data)
+        serializer.is_valid(raise_exception=True)
+        post = serializer.save()
         return HttpResponse(
             status=HTTPStatus.CREATED,
             headers={"Location": reverse("api_post_detail", args=(post.pk,))},
@@ -45,12 +49,14 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     if request.method == "GET":
-        return JsonResponse(post_to_dict(post))
+        return JsonResponse(PostSerializer(post).data)
     elif request.method == "PUT":
         post_data = json.loads(request.body)
         for field, value in post_data.items():
             setattr(post, field, value)
-        post.save()
+        serializer = PostSerializer(post, data=post_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return HttpResponse(status=HTTPStatus.NO_CONTENT)
     elif request.method == "DELETE":
         post.delete()
